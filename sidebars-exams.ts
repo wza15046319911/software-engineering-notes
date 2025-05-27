174f3b5ba3001a3bb167264c36dbba3c61a092e8
Zian Wang <913248383@qq.com>
2025-05-27 13:31:29 +1000

    feat: rebuild the left-hand-side bar for exam and exercise

--- sidebars-exams.ts
+++ sidebars-exams.ts
@@ -1,39 +1,39 @@
-import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 /**
  * 考试资料侧边栏配置
  */
 const sidebarExams: SidebarsConfig = {
-  examsSidebar: [
-    // {
-    //   type: 'doc',
-    //   id: 'index',
-    //   label: '考试资料概述',
-    // },
-    'index',
-    {
-      type: 'category',
-      label: '期中考试',
-      items: [
-        'midterm/2024s2',
-        'midterm/2024s1',
-        'midterm/practice',
-        'midterm/preparation',
-      ],
-    },
-    {
-      type: 'category',
-      label: '期末考试',
-      items: [
-        'final/2024s2',
-        'final/2024s1',
-        'final/2023s2',
-        'final/2023s1',
-        'final/2022s2',
-        'final/preparation',
-      ],
-    },
-  ],
+    examsSidebar: [
+        // {
+        //   type: 'doc',
+        //   id: 'index',
+        //   label: '考试资料概述',
+        // },
+        "index",
+        {
+            type: "category",
+            label: "期中考试",
+            items: [
+                "midterm/2024s2",
+                "midterm/2024s1",
+                "midterm/practice",
+                // 'midterm/preparation',
+            ],
+        },
+        {
+            type: "category",
+            label: "期末考试",
+            items: [
+                "final/2024s2",
+                "final/2024s1",
+                "final/2023s2",
+                "final/2023s1",
+                "final/2022s2",
+                // 'final/preparation',
+            ],
+        },
+    ],
 };
 
-export default sidebarExams; 
+export default sidebarExams;
